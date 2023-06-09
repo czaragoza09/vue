afbbfd5c58cc958d29647e558e938342742b5f1e
czaragoza09 <czaragoza@iesebre.com>
2023-06-09 08:37:45 +0200

    Afegim talles

--- main.js
+++ main.js
@@ -5,6 +5,12 @@ const app = Vue.createApp({
             image: './assets/images/socks_green.jpg',
             description: 'Calcetins per anar a la moda',
             inventory: 120,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            sizes: ['S', 'M', 'L', 'XL'],
+            variants: [
+              { id: 1001, color: 'verd cesped' },
+              { id: 1002, color: 'blau feo' },
+            ],
             onSale: true,
             url: 'https://luckysocks.es/'
 
