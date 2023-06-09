6a03a5cf4acb33777ec2f97ccbf49a5bca37967b
czaragoza09 <czaragoza@iesebre.com>
2023-06-09 09:00:54 +0200

    Canvi de nom als color perquè si no lo green i lo blue no se veuen desactivar botonet

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
 const app = Vue.createApp({
     data() {
         return {
+            cart: 0,
             product: 'Socks',
+            brand: 'Lucky Socks',
             image: './assets/images/socks_green.jpg',
+            instock: false,
             description: 'Calcetins per anar a la moda',
             inventory: 120,
             details: ['50% cotton', '30% wool', '20% polyester'],
             sizes: ['S', 'M', 'L', 'XL'],
             variants: [
-              { id: 1001, color: 'verd cesped',image: './assets/images/socks_green.jpg' },
-              { id: 1002, color: 'blau feo',image: './assets/images/socks_blue.jpg' },
+              { id: 1001, color: 'green',image: './assets/images/socks_green.jpg' },
+              { id: 1002, color: 'blue',image: './assets/images/socks_blue.jpg' },
             ],
             onSale: true,
             url: 'https://luckysocks.es/'
