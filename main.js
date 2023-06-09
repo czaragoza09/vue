6b3237304fa374871c2665e1650361b8d86fdf9d
czaragoza09 <czaragoza@iesebre.com>
2023-06-09 09:18:03 +0200

    Arreglar details i stock

--- main.js
+++ main.js
@@ -4,15 +4,15 @@ const app = Vue.createApp({
             cart: 0,
             product: 'Socks',
             brand: 'Lucky Socks',
-            image: './assets/images/socks_green.jpg',
+            selectedVariant: 0,
             instock: false,
             description: 'Calcetins per anar a la moda',
             inventory: 120,
             details: ['50% cotton', '30% wool', '20% polyester'],
             sizes: ['S', 'M', 'L', 'XL'],
             variants: [
-              { id: 1001, color: 'green',image: './assets/images/socks_green.jpg' },
-              { id: 1002, color: 'blue',image: './assets/images/socks_blue.jpg' },
+              { id: 1001, color: 'green',image: './assets/images/socks_green.jpg', quantity: 30 },
+              { id: 1002, color: 'blue',image: './assets/images/socks_blue.jpg', quantity:0},
             ],
             onSale: true,
             url: 'https://luckysocks.es/'
@@ -30,8 +30,19 @@ const app = Vue.createApp({
             }
         },
         // solution
-        updateImage(variantImage) {
-            this.image = variantImage
+        updateVariant(index) {
+            this.selectedVariant = index
+        }
+    },
+    computed:{
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+        instock() {
+            return this.variants[this.selectedVariant].quantity
         }
     }
 })
