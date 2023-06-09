1ebf8cd4b6ce5326a098be829371d13f2f809370
czaragoza09 <czaragoza@iesebre.com>
2023-06-09 08:47:58 +0200

    Canvi coloret al pasar lo ratoli

--- main.js
+++ main.js
@@ -8,12 +8,27 @@ const app = Vue.createApp({
             details: ['50% cotton', '30% wool', '20% polyester'],
             sizes: ['S', 'M', 'L', 'XL'],
             variants: [
-              { id: 1001, color: 'verd cesped' },
-              { id: 1002, color: 'blau feo' },
+              { id: 1001, color: 'verd cesped',image: './assets/images/socks_green.jpg' },
+              { id: 1002, color: 'blau feo',image: './assets/images/socks_blue.jpg' },
             ],
             onSale: true,
             url: 'https://luckysocks.es/'
 
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        // solution
+        removeFromCart() {
+            if (this.cart >= 1) {
+                this.cart -= 1
+            }
+        },
+        // solution
+        updateImage(variantImage) {
+            this.image = variantImage
         }
     }
 })
